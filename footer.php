5c014d7aefada700228885f48718cf76220671e2
nunna19 <nunnapat_ch@yahoo.com.com>
2019-05-22 14:51:44 -0400

    first

---
+++ footer.php
@@ -0,0 +1,18 @@
+
+
+    </div><!-- #content -->
+
+<footer id="colophon" class="site-footer" role="contentinfo">
+
+<a href=' <?php echo esc_url(__("https://github.com/nunna19", "wphierarchy")) ?> '>
+        <?php printf(esc_html__('this is github link', 'wphierarchy'), 'gitHub'); ?>
+    </a>
+
+</footer>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
